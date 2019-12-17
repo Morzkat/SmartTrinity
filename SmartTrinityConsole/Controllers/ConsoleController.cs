b2a9e8cce7bf5590de4994bebe384d16af3dddff
Felix David Garcia <felix.david21@outlook.com>
2019-12-17 11:10:48 -0400

    Test UOF and remove unnecessary packages

--- SmartTrinityConsole/Controllers/ConsoleController.cs
+++ SmartTrinityConsole/Controllers/ConsoleController.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using SmartTrinityApi.Core.Interfaces.Services;
 using System.Collections.Generic;
 using SmartTrinityConsole.Core.Entities.Sale;
+using SmartTrinityApi.Core.Interfaces.UnitOfWork;
 
 namespace SmartTrinityApi.Controllers
 {
@@ -12,10 +13,11 @@ namespace SmartTrinityApi.Controllers
     {
         // IMainService _mainService;
         ILogger<ConsoleController> _logger;
-
-        public ConsoleController(ILogger<ConsoleController> logger)
+        IUnitOfWork _unitOfWork;
+        public ConsoleController(ILogger<ConsoleController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         /*
@@ -29,6 +31,7 @@ namespace SmartTrinityApi.Controllers
         [HttpGet("Test")]
         public ActionResult<IList<IList<Sale>>> Test()
         {
+            var p = _unitOfWork.ConfigValuesRepository.Get(2);
             return new List<IList<Sale>>();
         }
     }
