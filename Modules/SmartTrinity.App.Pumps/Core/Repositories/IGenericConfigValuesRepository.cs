668ecce95f0fa7fa8ee44ab1eb7ced3bc4db9b21
Morzkat <felix.david21@outlook.com>
2024-05-28 08:43:32 -0400

    * feat: Improve logic for update the pump service mode

--- Modules/SmartTrinity.App.Pumps/Core/Repositories/IGenericConfigValuesRepository.cs
+++ Modules/SmartTrinity.App.Pumps/Core/Repositories/IGenericConfigValuesRepository.cs
@@ -6,5 +6,7 @@ namespace SmartTrinity.App.Pumps.Core.Repositories
     public interface IGenericConfigValuesRepository : IRepository<GenericConfigValues>
     {
         Task<bool> UpdatePumpServiceMode(ServiceMode serviceMode);
+
+        Task<GenericConfigValues> GetServiceModeByDeviceId(int id);
     }
 }
